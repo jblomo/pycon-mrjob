4e0f6dc418f15acc2380a76a4256d2fca79c07ea
Jim Blomo <jim.blomo+github@gmail.com>
2014-04-07 23:44:20 -0700

    ex2 slides

--- code/top_pages.py
+++ code/top_pages.py
@@ -14,7 +14,12 @@ To store output:
 """
 
 from mrjob.job import MRJob
-from combine_user_visits import csv_readline
+import csv
+
+def csv_readline(line):
+    """Given a sting CSV line, return a list of strings."""
+    for row in csv.reader([line]):
+        return row
 
 class TopPages(MRJob):
 
