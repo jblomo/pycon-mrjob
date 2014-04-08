a202db3521299b5e1c7a1a6ac611456b3d81e8e4
Jim Blomo <jim.blomo+github@gmail.com>
2014-04-07 23:58:48 -0700

    ex2 solution

--- code/count_titles.py
+++ code/count_titles.py
@@ -23,10 +23,10 @@ class CountTitles(MRJob):
         """Extracts the Vroot that was visited"""
         cell = csv_readline(line)
         if cell[0] == 'V':
-            yield ### FILL IN
+            yield cell[1], ('V', 1)
                   # How to "tag" this value for a given Key?
-        elif cell[0] == 'A'
-            yield ### FILL IN
+        elif cell[0] == 'A':
+            yield cell[1], ('A', cell[3])
                   # How to "tag" this value for a given Key?
 
     def reducer(self, vroot, visit_counts_and_title):
@@ -36,10 +36,10 @@ class CountTitles(MRJob):
         title = ''
 
         for value in visit_counts_and_title:
-            if # FILL IN: value is a visit type:
-                total += # FILL IN: extract untagged value
-            elif # FILL IN: value is a attribute type:
-                title = # FILL IN: extract untagged title
+            if value[0] == 'V':
+                total += value[1]
+            elif value[0] == 'A':
+                title = value[1]
 
         yield title, total
         
--- code/top_titles.py
+++ code/top_titles.py
@@ -1,21 +1,13 @@
-"""Find the titles of the top visited Vroots.
-
-This program will take a CSV data file and output lines of
-
-    Vroot Title
-
-To run:
-
-    python top_titles.py anonymous-msweb.data
+"""Run an MRJob to generate the counts for all titles, then sort the results
+and show the top 10.
 """
 
 from count_titles import CountTitles
-import csv
 
 mr_job = CountTitles(args=['-r', 'local', 'msanon/anonymous-msweb.data.gz'])
 with mr_job.make_runner() as runner:
     runner.run()
     title_counts = [mr_job.parse_output_line(line) for line in
             runner.stream_output()]
-    results = sorted(title_counts, key=lambda (k,v): v)
+    results = sorted(title_counts, key=lambda (k,v): v, reverse=True)
     print results[:10]
