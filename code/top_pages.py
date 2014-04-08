da2a6f30b903ac65b648bea452ec18ac744f3a2b
Jim Blomo <jim.blomo+github@gmail.com>
2014-04-07 22:33:36 -0700

    exercise solution

--- code/top_pages.py
+++ code/top_pages.py
@@ -14,7 +14,12 @@ To store output:
 """
 
 from mrjob.job import MRJob
-from combine_user_visits import csv_readline
+import csv
+
+def csv_readline(line):
+    """Given a sting CSV line, return a list of strings."""
+    for row in csv.reader([line]):
+        return row
 
 class TopPages(MRJob):
 
@@ -22,16 +27,16 @@ class TopPages(MRJob):
         """Extracts the Vroot that was visited"""
         cell = csv_readline(line)
         if cell[0] == 'V':
-            yield ### FILL IN
+            yield cell[1], 1
                   # What  Key, Value  do we want to output?
 
     def reducer(self, vroot, visit_counts):
         """Sumarizes the visit counts by adding them together.  If total visits
         is more than 400, yield the results"""
-        total = ### FILL IN
+        total = sum(visit_counts)
                 # How do we calculate the total visits from the visit_counts?
         if total > 400:
-            yield ### FILL IN
+            yield vroot, total
                   # What  Key, Value  do we want to output?
         
 if __name__ == '__main__':
